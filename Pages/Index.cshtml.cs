3d9faf04bf332d9321155662c60e70d28a55952d
Gabriela Malinowska <pysia24.20@onet.pl>
2021-03-16 09:47:04 +0100

    Dodaj pliki projektów.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ps3.forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ps3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public Adress Adress { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "User";
+        }
+        /*public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return RedirectToPage("./Privacy");
+            return Page();      
+        }*/
+
+    }
+}
---
+++ forms/Adress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ps3.forms
+{
+    public class Adress
+    {
+        [Display(Name = "Ulica")]
+        [StringLength(60,MinimumLength = 3), Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztowy")]
+        [StringLength(6,MinimumLength = 6), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string City { get; set; }
+        [Display(Name = "Numer")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int Number { get; set; }
+    }
+}
